48c2f461da49f791a1425dee8931010e790267bb
Cory Martin <murribu@gmail.com>
2015-06-29 16:17:56 -0500

    POST /v1/teams (update)

--- app/controllers/v1/teams_controller.rb
+++ app/controllers/v1/teams_controller.rb
@@ -1,27 +1,27 @@
 module V1
   class TeamsController < ApplicationController
     #POST /v1/teams
-    #expect params[:site, :sport, :league_id, :team_id, :team_name, :league_name]
-    #if the team already exists, edit. If not, create. Either way, return the team's info
+    #expect params[:site, :sport, :league_id, :team_id, :team_name, :league_name, :team_id]
+    #if the team already exists, you can edit the team_name. If not, create. Either way, return the team's info
     def create
-      sport = Sport.where("name = ? or abbr = ? or id = ?", params[:sport], params[:sport], params[:sport].to_i)
-      site = FantasySite.where("name = ? or abbr = ? or id = ?", params[:site], params[:site], params[:site].to_i)
-      if sport.length != 1
-        render json: {"error" => "invalid sport. This is benchwarner's fault, please yell at us"}, status: 403
+      team = FantasyTeam.where({id: params[:team_id]})
+      if team.length > 0
+        #existing team
+        team[0].update(:name => params[:team_name])
       else
-        sport = sport[0]
-      end
-      if site.length != 1
-        response['Accept'] = 'Yahoo!,ESPN'
-        render json: {"error" => "invalid site"}, status: 405
-      else
-        site = site[0]
-        league = FantasyLeague.where({site_id: site["id"], site_generated_id: params[:league_id]})
-        team = FantasyTeam.includes(:fantasy_league).where(fantasy_leagues: {site_id: site["id"], site_generated_id: params[:league_id]}, fantasy_teams: {site_generated_id: params[:team_id]})
-        if team.length > 0
-          #existing team
+        sport = Sport.where("name = ? or abbr = ? or id = ?", params[:sport], params[:sport], params[:sport].to_i)
+        site = FantasySite.where("name = ? or abbr = ? or id = ?", params[:site], params[:site], params[:site].to_i)
+        if sport.length != 1
+          render json: {"error" => "invalid sport. This is benchwarner's fault, please yell at us"}, status: 403
         else
-          #new team
+          sport = sport[0]
+        end
+        if site.length != 1
+          response['Accept'] = 'Yahoo!,ESPN'
+          render json: {"error" => "invalid site"}, status: 405
+        else
+          site = site[0]
+          league = FantasyLeague.where({site_id: site["id"], site_generated_id: params[:league_id]})
           if league.length > 0
             #existing league
           else
@@ -29,8 +29,8 @@ module V1
           end
           team = FantasyTeam.create(:user_id => @auth_user[:id], :name => params[:team_name], :site_generated_id => params[:team_id], :fantasy_league_id => league["id"])
         end
-        render json: team
       end
+      render json: team
     end
   end
 end
--- spec/requests/v1/fantasy_team_spec.rb
+++ spec/requests/v1/fantasy_team_spec.rb
@@ -12,4 +12,12 @@ describe "For Fantasy teams," do
       expect(response.body).to match(/Dudan/)
     end
   end
+  describe "editing a team_name" do
+    it "returns the team's info" do
+      post_with_token "/v1/teams", {"site" => "Yahoo!", "sport" => "Baseball", "league_id" => "1234", "team_id" => "4", "team_name" => "Oh no you Dudan't", "league_name" => "Awesome League"}, {'Authorization' => @user_auth_token}
+    arr = JSON.parse(response.body)
+    post_with_token "/v1/teams", {"team_id" => arr["id"], "team_name" => "Melvin Diagram"}, {'Authorization' => @user_auth_token}
+      expect(response.body).to match(/Melvin/)
+    end
+  end
 end
